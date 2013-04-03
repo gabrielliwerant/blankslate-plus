9d6ee7cc0ef04749a019607838f5ad8a287469e0
Gabriel <gabriel@abrightconcept.com>
2013-04-03 04:14:01 -0400

    Remove scripts from footer and erroneous styles from 650-and-up.less

--- footer.php
+++ footer.php
@@ -16,25 +16,6 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/Javascript">
-_lf_cid = "LF_a155784c";
-(function(){var _cid = _lf_cid;var load = function() {var s, p;var m = 10;var c = 0;s = document.createElement('script');s.type = 'text/javascript';s.async = true;if (window.location.protocol.indexOf('https:') == 0) {s.src = 'https://c368677.ssl.cf1.rackcdn.com/loopfuse_listener.js';} else {s.src = 'http://static.lfov.net/loopfuse_listener.js';}p = document.getElementsByTagName('script')[0];p.parentNode.insertBefore(s, p);var remora = function() {if (c < m) {c++;if (typeof _lf_remora !== 'undefined') {_lf_cid = _cid;_lf_remora();} else {setTimeout(function() { remora() }, 500);}}};remora();};if (window.attachEvent) {window.attachEvent('onload', load);} else {window.addEventListener('load', load, false);}})();
-</script>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-5835752-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
 </body>
 
 </html>
